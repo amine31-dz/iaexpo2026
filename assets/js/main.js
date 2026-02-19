f87a84d6789215761b36fb48626b7eb924260c29
amine31-dz <237440332+amine31-dz@users.noreply.github.com>
2026-02-20 00:06:28 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,3 +53,19 @@ document.addEventListener("DOMContentLoaded", () => {
       if (map[t]) a.href = map[t];
     });
 });
+const targetDate = new Date("October 12, 2026 09:00:00").getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const diff = targetDate - now;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+}, 1000);
